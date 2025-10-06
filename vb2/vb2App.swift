63b3012f86b3254cf672b71f4c813bf9aa913225
Claude Wilder <claude.wilder@gmail.com>
2025-10-06 18:41:57 -0400

    Initial Commit

---
+++ vb2/vb2App.swift
@@ -0,0 +1,17 @@
+//
+//  vb2App.swift
+//  vb2
+//
+//  Created by Claude Wilder on 2025-10-06.
+//
+
+import SwiftUI
+
+@main
+struct vb2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vb2Tests/vb2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  vb2Tests.swift
+//  vb2Tests
+//
+//  Created by Claude Wilder on 2025-10-06.
+//
+
+import Testing
+@testable import vb2
+
+struct vb2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
